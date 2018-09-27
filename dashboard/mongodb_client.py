492b2740ad328fde613ae88fe5f82954468e5c32
horis233 <horis233@gmail.com>
2018-09-27 13:04:55 -0400

    added flask backend

---
+++ dashboard/mongodb_client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+""" Mongo Client """
+from pymongo import MongoClient
+
+# get config
+# import config_client
+MONGO_DB_URL = "mongodb://horis233:horis233@ds029496.mlab.com:29496/horis_tapnews"
+MONGO_DB_HOST = "ds029496.mlab.com"
+MONGO_DB_PORT = 29496
+DB_NAME = "horis_tapnews"
+DB_USER = "horis233"
+DB_PASS = "horis233"
+
+client = MongoClient(MONGO_DB_HOST, MONGO_DB_PORT)
+
+
+def get_db(db=DB_NAME):
+    db = client[db]
+    db.authenticate(DB_USER, DB_PASS)
+    return db
